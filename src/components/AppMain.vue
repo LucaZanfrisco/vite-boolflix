74c9c89b21021ac6075486cf166663331a8fcf2d
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-03-29 21:10:09 +0200

    Aggiunta di font awesome per le icone

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,23 +10,26 @@ export default {
     methods:{
         flag(movie){
             if(movie.original_language === 'en'){
-                return `${store.config.apiFlag}/US/flat/16.png`
+                return `${store.config.apiFlag}/US/flat/16.png`;
             }else if(movie.original_language === 'ja'){
-                return `${store.config.apiFlag}/JP/flat/16.png`
+                return `${store.config.apiFlag}/JP/flat/16.png`;
             }else if(movie.original_language === 'ko'){
-                return `${store.config.apiFlag}/KR/flat/16.png`
+                return `${store.config.apiFlag}/KR/flat/16.png`;
             }else if(movie.original_language === 'zh'){
-                return `${store.config.apiFlag}/CN/flat/16.png`    
+                return `${store.config.apiFlag}/CN/flat/16.png`;    
             }else if(movie.original_language === 'da'){
-                return `${store.config.apiFlag}/DK/flat/16.png`
+                return `${store.config.apiFlag}/DK/flat/16.png`;
             }else{
-                return `${store.config.apiFlag}/${movie.original_language.toUpperCase()}/flat/16.png`
+                return `${store.config.apiFlag}/${movie.original_language.toUpperCase()}/flat/16.png`;
             }
         },
         poster(movie){
-            return `${store.config.apiImage}${movie.poster_path}`
+            return `${store.config.apiImage}${movie.poster_path}`;
+        },
+        stars(movie){
+            return Math.ceil(movie.vote_average / 5); 
         }
-    }
+    },
 }
 </script>
 <template lang="">
@@ -37,7 +40,7 @@ export default {
                 <div>{{ movie.title }}</div>
                 <div>{{ movie.original_title }}</div>
                 <div><img :src="flag(movie)" :alt="flag(movie)"></div>
-                <div>{{ movie.vote_average }}</div>
+                <div v-for="n in 5"><font-awesome-icon icon="fa-solid fa-star" /> {{ stars(movie) }}</div>
                 <div>------</div>
             </li>
         </ul>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
-
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import './assets/scss/main.scss';
+library.add(faStar);
 
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
