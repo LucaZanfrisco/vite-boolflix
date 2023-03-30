5eb82c123b97789692c56e8c6c2e2822eaba99d2
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-03-30 16:38:58 +0200

    Aggiunto stile header

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue';
+
+// Import FontAwesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
-import './assets/scss/main.scss';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import * as bootstrap from 'bootstrap'
-library.add(faStar);
+library.add(faStar,faBars);
+// ------
+// Import SCSS
+import './assets/scss/main.scss';
+// -----
 
 import App from './App.vue'
-
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
