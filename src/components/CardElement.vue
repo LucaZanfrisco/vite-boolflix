6879cb31f5b2d4b48063938ef2af0a1881734476
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-03-30 17:34:53 +0200

    Effetto hover sui poster che mostra le info

--- src/components/CardElement.vue
+++ src/components/CardElement.vue
@@ -12,19 +12,46 @@ export default {
 }
 </script>
 <template>
-    <div>
-        <div><img :src="poster" :alt="title" class="img-fluid border border border-opacity-25 border-light"></div>
-        <div>{{ title }}</div>
-        <div>{{ original_title }}</div>
-        <div><img :src="flag" :alt="language" class="img-fluid"></div>
-        <div >
-            <font-awesome-icon icon="fa-solid fa-star" class="vote" v-for="n in stars"/>
-            <font-awesome-icon icon="fa-solid fa-star" v-for="n in 5-stars"/>
+        <div class="poster">
+            <img :src="poster" :alt="title" class="img-fluid border border border-opacity-25 border-light poster-path">
+            <div class="info">
+                <div v-if="title == original_title" class="fs-4">{{ title }}</div>
+                <div v-else>
+                    <div class="fs-4 mb-3">{{ title }}</div>
+                    <div>{{ original_title }}</div>
+                </div>
+                <div><img :src="flag" :alt="language" class="img-fluid"></div>
+                <div>
+                    <font-awesome-icon icon="fa-solid fa-star" class="vote" v-for="n in stars" />
+                    <font-awesome-icon icon="fa-solid fa-star" v-for="n in 5 - stars" />
+                </div>
+            </div>
         </div>
-    </div>
 </template>
 <style lang="scss" scoped>
-.vote{
+.vote {
     color: gold;
+}
+
+.poster {
+    position: relative;
+    cursor: pointer;
+    overflow-y: auto;
+    &:hover .poster-path{
+        opacity: 25%;
+    }
+    &:hover .info{
+        
+        display: block;
+    }
+}
+
+.info {
+    position: absolute;
+    top: 10%;
+    left: 10%;
+    transform: translate(-10%,-10%);
+    padding: .625rem;
+    display: none;
 }
 </style>
