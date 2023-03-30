f30b3d3f79c530adb04e097f4065c843e0d514ff
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-03-30 16:39:22 +0200

    Aggiunto stile header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,28 +9,47 @@ export default {
   },
 };
 </script>
-<template lang="">
-  <header>
-    <div>
-      <div>
-        <img :src="store.config.logoNetflix" alt="logo" class="img-fluid"/>
-      </div>
-      <div>
-        <ul>
-            <li>Home</li>
-            <li>Film</li>
-            <li>TV Series</li>
-            <li>Recently Added</li>
-            <li>My List</li>
-        </ul>
-      </div>
-      
-      <div>
-        <div>
-          <input type="text" v-model="store.searchName" minlength="1" placeholder="Cerca film o serie tv" class="form-control text-center"/>
-          <button type="button" @click="$emit('search')" class="btn btn-secondary">Cerca</button>
+<template >
+  <header class="border-bottom border-danger border-2">
+    <div class="container">
+      <div class="row justify-content-center align-items-lg-center">
+        <div class="col-12 col-lg-1 text-center mb-3 mb-lg-0">
+          <img :src="store.config.logoNetflix" alt="logo" class="img-fluid" />
+        </div>
+        <div class="col-12 col-lg-auto d-none d-md-block">
+          <ul class="row justify-content-center mb-3 mb-lg-0">
+            <li class="col-auto">Home</li>
+            <li class="col-auto">Film</li>
+            <li class="col-auto">Series</li>
+            <li class="col-auto">Recently</li>
+            <li class="col-auto">My List</li>
+          </ul>
+        </div>
+        <div class="col-12 col-lg-3">
+          <div class="row row-cols-3 align-items-center justify-content-between">
+            <div class="dropdown col-2 col-sm-1 col-md-4 d-md-none">
+              <button class="btn btn-danger me-0" type="button" data-bs-toggle="dropdown">
+                <font-awesome-icon icon="fa-solid fa-bars" />
+              </button>
+              <ul class="dropdown-menu">
+                <li>Home</li>
+                <li>Film</li>
+                <li>TV Series</li>
+                <li>Recently</li>
+                <li>My List</li>
+              </ul>
+            </div>
+            <div class="col-8 col-sm-10">
+              <input type="text" v-model="store.searchName" minlength="1" placeholder="Cerca film o serie tv"
+              class="form-control" />
+            </div>
+            <div class="col-2 col-sm-1">
+              <button type="button" @click="$emit('search')" class="btn btn-danger">Cerca</button>
+            </div>
+          </div>
         </div>
       </div>
+
     </div>
     <!-- <div>Film/Serie Trovati: {{ store.generalList.general.length }}</div> -->
   </header>
@@ -38,16 +57,14 @@ export default {
 
 <style lang="scss" scoped>
 header {
-  height: 100px;
   padding: 1.25rem;
 
   li {
     cursor: pointer;
     border-radius: 10px;
     margin: 0 .9375rem;
-
     &:hover {
-      text-decoration: underline 5px white;
+      text-decoration: underline 3px;
     }
   }
 }
